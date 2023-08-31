4a18125458d734b0beda361327ca243bf0ebb7cb
venkata.gandham <venkata.gandham@pennywisesolutions.com>
2023-08-31 16:04:38 +0530

    Pushing latest updates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import IndexLayout from './components/Index.vue'
 </script>
 
@@ -9,7 +8,7 @@ import IndexLayout from './components/Index.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hello World !" />
     </div>
   </header>
 
